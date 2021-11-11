b3533815b8fed21124e20587cb7804edee6f53d0
DimitarKisov <mitko9737@gmail.com>
2021-11-11 14:57:10 +0200

    Added Serilog to Identity microservice and Result.cs for easy result returning with message in every controller

--- Body4U.Identity/Program.cs
+++ Body4U.Identity/Program.cs
@@ -1,12 +1,37 @@
 namespace Body4U.Identity
 {
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using System;
 
     public class Program
     {
         public static void Main(string[] args)
-            => CreateHostBuilder(args).Build().Run();
+        {
+            var configuration = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Body4U.Identity failed to start!");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
