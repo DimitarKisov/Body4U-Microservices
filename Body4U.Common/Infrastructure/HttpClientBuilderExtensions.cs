0a26dffe3aee09543af11c17eb7ee788463d3359
DimitarKisov <mitko9737@gmail.com>
2022-01-08 16:37:03 +0200

    Added message resiliency between microservices communication

--- Body4U.Common/Infrastructure/HttpClientBuilderExtensions.cs
+++ Body4U.Common/Infrastructure/HttpClientBuilderExtensions.cs
@@ -3,15 +3,16 @@
     using Body4U.Common.Services.Identity;
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.DependencyInjection;
+    using Polly;
     using System;
+    using System.Net;
     using System.Net.Http.Headers;
 
     public static class HttpClientBuilderExtensions
     {
-        public static void WithConfiguration(
-            this IHttpClientBuilder httpClientBuilder,
-            string baseAddress)
-            => httpClientBuilder
+        public static void WithConfiguration(this IHttpClientBuilder httpClientBuilder, string baseAddress)
+        {
+            httpClientBuilder
                 .ConfigureHttpClient((serviceProvider, client) =>
                 {
                     client.BaseAddress = new Uri(baseAddress);
@@ -32,6 +33,13 @@
 
                     var authorizationHeader = new AuthenticationHeaderValue("Bearer", currentToken);
                     client.DefaultRequestHeaders.Authorization = authorizationHeader;
-                });
+                })
+                .AddTransientHttpErrorPolicy(policy => policy //Add Http policy for errors
+                    .OrResult(result => result.StatusCode == HttpStatusCode.NotFound) //At result NotFound or anything else
+                    .WaitAndRetryAsync(5, retry => //Wait and try again 5 times
+                        TimeSpan.FromSeconds(Math.Pow(2, retry)))) //The first will be 2 sec, second will be 2^2, third 4^2, forth will be 16^2 and the last 256 ^ 2
+                .AddTransientHttpErrorPolicy(policy => policy //Add Http policy for errors
+                    .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30))); //Try max 5 times before to stop trying for 30 seconds
+        }
     }
 }
