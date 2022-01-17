09093368250f484bcc5088343582c097e02f6ee1
DimitarKisov <mitko9737@gmail.com>
2022-01-17 10:09:09 +0200

    test rework branch

--- Body4U.Common/Controllers/ApiController.cs
+++ Body4U.Common/Controllers/ApiController.cs
@@ -1,6 +1,8 @@
 ﻿namespace Body4U.Common.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+    using System.Net;
 
     [ApiController]
     [Route("[controller]")]
@@ -10,5 +12,34 @@
         protected const string Id = "{id}";
         protected const string TrainerId = "{trainerId}";
         protected const string Term = "{term}";
+
+        protected ActionResult ProcessErrors(Result<object> result)
+        {
+            this.ModelState.Clear();
+
+            if (result.StatusCode == HttpStatusCode.BadRequest)
+            {
+                return this.BadRequest(result.Errors);
+            }
+            else if (result.StatusCode == HttpStatusCode.Forbidden)
+            {
+                return this.Forbid();
+            }
+            else if (result.StatusCode == HttpStatusCode.NotFound)
+            {
+                return this.NotFound(result.Errors);
+            }
+            else if (result.StatusCode == HttpStatusCode.Conflict)
+            {
+                return this.Conflict(result.Errors);
+            }
+            else if (result.StatusCode == HttpStatusCode.InternalServerError)
+            {
+                return this.Problem(result.Errors.First());
+            }
+
+            return this.BadRequest(result.Errors);
+        }
     }
+}
 }
--- Body4U.Common/Result.cs
+++ Body4U.Common/Result.cs
@@ -3,15 +3,17 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net;
 
     public class Result
     {
         private readonly List<string> errors;
 
-        public Result(bool succeeded, List<string> errors)
+        public Result(bool succeeded, List<string> errors, HttpStatusCode statusCode)
         {
             this.Succeeded = succeeded;
             this.errors = errors;
+            this.StatusCode = statusCode;
         }
 
         public bool Succeeded { get; }
@@ -21,23 +23,25 @@
                 ? new List<string>()
                 : this.errors;
 
+        public HttpStatusCode StatusCode { get; }
+
         public static Result Success
-            => new Result(true, new List<string>());
+            => new Result(true, new List<string>(), new HttpStatusCode());
 
-        public static Result Failure(IEnumerable<string> errors)
-            => new Result(false, errors.ToList());
+        public static Result Failure(HttpStatusCode statusCode, IEnumerable<string> errors)
+            => new Result(false, errors.ToList(), new HttpStatusCode());
 
-        public static Result Failure(string error)
-            => new Result(false, new List<string> { error });
+        public static Result Failure(HttpStatusCode statusCode, string error)
+            => new Result(false, new List<string> { error }, new HttpStatusCode());
 
         public static implicit operator Result(string error)
-            => Failure(new List<string> { error });
+            => Failure(new HttpStatusCode(), new List<string> { error });
 
         public static implicit operator Result(List<string> errors)
-            => Failure(errors.ToList());
+            => Failure(new HttpStatusCode(), errors.ToList());
 
         public static implicit operator Result(bool success)
-            => success ? Success : Failure(new[] { "Unsuccessful operation." });
+            => success ? Success : Failure(new HttpStatusCode(), new[] { "Unsuccessful operation." });
 
         public static implicit operator bool(Result result)
             => result.Succeeded;
@@ -47,8 +51,8 @@
     {
         private readonly TData data;
 
-        private Result(bool succeeded, TData data, List<string> errors)
-            : base(succeeded, errors)
+        private Result(bool succeeded, TData data, List<string> errors, HttpStatusCode statusCode)
+            : base(succeeded, errors, new HttpStatusCode())
             => this.data = data;
 
         public TData Data
@@ -58,19 +62,19 @@
                     $"{nameof(this.Data)} is not available with a failed result. Use {this.Errors} instead.");
 
         public static Result<TData> SuccessWith(TData data)
-            => new Result<TData>(true, data, new List<string>());
+            => new Result<TData>(true, data, new List<string>(), new HttpStatusCode());
 
-        public new static Result<TData> Failure(IEnumerable<string> errors)
-            => new Result<TData>(false, default!, errors.ToList());
+        public new static Result<TData> Failure(HttpStatusCode statusCode, IEnumerable<string> errors)
+            => new Result<TData>(false, default!, errors.ToList(), new HttpStatusCode());
 
         public new static Result<TData> Failure(string error)
-            => new Result<TData>(false, default!, new List<string> { error });
+            => new Result<TData>(false, default!, new List<string> { error }, new HttpStatusCode());
 
         public static implicit operator Result<TData>(string error)
-            => Failure(new List<string> { error });
+            => Failure(new HttpStatusCode(), new List<string> { error });
 
         public static implicit operator Result<TData>(List<string> errors)
-            => Failure(errors);
+            => Failure(new HttpStatusCode(), errors);
 
         public static implicit operator Result<TData>(TData data)
             => SuccessWith(data);
